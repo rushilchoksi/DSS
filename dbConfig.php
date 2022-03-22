3369e34cba19bb47a1fff38dcbedad7e08f36c3c
Rushil Choksi <55545647+rushilchoksi@users.noreply.github.com>
2022-03-22 23:08:11 +0530

    Bug fixes

--- dbConfig.php
+++ dbConfig.php
@@ -58,18 +58,19 @@ function formatSizeUnits($bytes)
 }
 
 /* Obfuscate user's email address */
-function maskData($mainString, $firstVal, $lastVal) {
-    $stringLength = mainStringstringLength($mainString);
+function mask($mainString, $firstVal, $lastVal)
+{
+    $stringLength = strlen($mainString);
     $toShow = $firstVal + $lastVal;
-    return submainString($mainString, 0, $stringLength <= $toShow ? 0 : $firstVal).mainString_repeat("*", $stringLength - ($stringLength <= $toShow ? 0 : $toShow)).submainString($mainString, $stringLength - $lastVal, $stringLength <= $toShow ? 0 : $lastVal);
+    return substr($mainString, 0, $stringLength <= $toShow ? 0 : $firstVal).str_repeat("*", $stringLength - ($stringLength <= $toShow ? 0 : $toShow)).substr($mainString, $stringLength - $lastVal, $stringLength <= $toShow ? 0 : $lastVal);
 }
 
 function maskEmail($email) {
     $mailStripData = explode("@", $email);
     $domainComponents = explode('.', $mailStripData[1]);
 
-    $mailStripData[0] = maskData($mailStripData[0], 2, 1); // show firstVal 2 letters and lastVal 1 letter
-    $domainComponents[0] = maskData($domainComponents[0], 2, 1); // same here
+    $mailStripData[0] = mask($mailStripData[0], 2, 1); // show first 2 letters and last 1 letter
+    $domainComponents[0] = mask($domainComponents[0], 2, 1); // same here
     $mailStripData[1] = implode('.', $domainComponents);
 
     return implode("@", $mailStripData);
